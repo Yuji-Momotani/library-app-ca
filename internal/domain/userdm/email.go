f7bbc8d0e95cd61cf21d2b8fece2bbe7ed2217b0
Yuji-Momotani <yujimomotani@yujinoMac-mini.local>
2026-05-26 08:21:16 +0900

    emailのvalueobject作成

--- internal/domain/userdm/email.go
+++ internal/domain/userdm/email.go
@@ -1,1 +1,48 @@
 package userdm
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type Email struct {
+	value string
+}
+
+var ErrInvalidEmailFormat = errors.New("invalid email format")
+
+func NewEmail(v string) (*Email, error) {
+	match, err := regexp.MatchString(
+		`^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$`,
+		v,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("メールアドレスがフォーマット検証に失敗しました: %w", err)
+	}
+	if !match {
+		return nil, fmt.Errorf(
+			"無効なメールアドレスフォーマット:%w :%s",
+			ErrInvalidEmailFormat,
+			v)
+	}
+
+	return &Email{
+		value: v,
+	}, nil
+}
+
+func (v *Email) Value() string {
+	return v.value
+}
+
+func (v *Email) Domain() string {
+	parts := strings.Split(v.value, "@")
+
+	return parts[1]
+}
+
+func (v *Email) Equal(other Email) bool {
+	return v.value == other.value
+}
--- internal/domain/userdm/email_test.go
+++ internal/domain/userdm/email_test.go
@@ -1,1 +1,75 @@
 package userdm_test
+
+import (
+	"testing"
+
+	"github.com/Yuji-Momotani/library-app-ca/internal/domain/userdm"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewEmail(t *testing.T) {
+	tests := []struct {
+		name        string
+		v           string
+		expected    string
+		wantErr     bool
+		expectedErr error
+	}{
+		{
+			name:     "正常系: 正しいメールアドレスフォーマット",
+			v:        "test@example.com",
+			expected: "test@example.com",
+			wantErr:  false,
+		},
+		{
+			name:        "異常系: メールアドレスフォーマットが正しくない",
+			v:           "test1234",
+			wantErr:     true,
+			expectedErr: userdm.ErrInvalidEmailFormat,
+		},
+		{
+			name:        "異常系: @で終わる",
+			v:           "test1234@",
+			wantErr:     true,
+			expectedErr: userdm.ErrInvalidEmailFormat,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := userdm.NewEmail(tt.v)
+			if tt.wantErr {
+				assert.ErrorIs(t, err, tt.expectedErr)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expected, actual.Value())
+			}
+		})
+	}
+}
+
+func TestDomain(t *testing.T) {
+	tests := []struct {
+		name     string
+		v        string
+		expected string
+	}{
+		{
+			name:     "ドメイン取得チェック",
+			v:        "test@example.com",
+			expected: "example.com",
+		},
+		{
+			name:     "ドメイン取得チェック",
+			v:        "test@hoge.co.jp",
+			expected: "hoge.co.jp",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			email, _ := userdm.NewEmail(tt.v)
+			assert.Equal(t, tt.expected, email.Domain())
+		})
+	}
+}
