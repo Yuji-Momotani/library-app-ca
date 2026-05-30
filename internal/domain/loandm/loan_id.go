dac556bafa8900d2c934152408c9686f8c1e052c
Yuji-Momotani <yujimomotani@yujinoMac-mini.local>
2026-05-30 09:04:30 +0900

    id関連作成・voの作成完了

--- internal/domain/loandm/loan_id.go
+++ internal/domain/loandm/loan_id.go
@@ -1,1 +1,41 @@
 package loandm
+
+import (
+	"fmt"
+
+	"github.com/Yuji-Momotani/library-app-ca/internal/domain/shared"
+	"github.com/oklog/ulid/v2"
+)
+
+type LoanID shared.ID
+
+func NewLoanID() (*LoanID, error) {
+	id, err := shared.NewID()
+	if err != nil {
+		return nil, fmt.Errorf("failed to shared NewID: %w", err)
+	}
+
+	lID := LoanID(id)
+
+	return &lID, nil
+}
+
+func LoanIDFromString(v string) (*LoanID, error) {
+	sid, err := ulid.Parse(v)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ulid parse:%w", err)
+	}
+
+	id := shared.IDFromULID(sid)
+	LoanID := LoanID(id)
+
+	return &LoanID, nil
+}
+
+func (v LoanID) Value() string {
+	return shared.ID(v).String()
+}
+
+func (v LoanID) Equal(other LoanID) bool {
+	return shared.ID(v).Equal(shared.ID(other))
+}
--- internal/domain/userdm/user_id.go
+++ internal/domain/userdm/user_id.go
@@ -1,1 +1,41 @@
 package userdm
+
+import (
+	"fmt"
+
+	"github.com/Yuji-Momotani/library-app-ca/internal/domain/shared"
+	"github.com/oklog/ulid/v2"
+)
+
+type UserID shared.ID
+
+func NewID() (*UserID, error) {
+	id, err := shared.NewID()
+	if err != nil {
+		return nil, err
+	}
+
+	userID := UserID(id)
+
+	return &userID, nil
+}
+
+func UserIDFromString(v string) (*UserID, error) {
+	id, err := ulid.Parse(v)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ulid parse:%w", err)
+	}
+
+	sid := shared.IDFromULID(id)
+	uid := UserID(sid)
+
+	return &uid, nil
+}
+
+func (v UserID) Value() string {
+	return shared.ID(v).String()
+}
+
+func (v UserID) Equals(other UserID) bool {
+	return shared.ID(v).Equal(shared.ID(other))
+}
